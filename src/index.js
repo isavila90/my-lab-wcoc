1f64d8af2be59dea76ade207c770c28d80f33acc
isabelladavilalares <avilalares.i@husky.neu.edu>
2021-05-31 21:11:53 -0400

    Lesson 1 completed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/MoreSugar-Regular.ttf';
+import './fonts/glacial-indifference.regular.otf';
+import './fonts/MoreSugar-Extras.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
